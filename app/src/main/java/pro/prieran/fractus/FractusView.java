5afd730973cc26620d24cf1d0466739be4e02945
prieran <prieran@ya.ru>
2017-02-17 17:29:45 +0300

    Second commit

--- app/src/main/java/pro/prieran/fractus/FractusView.java
+++ app/src/main/java/pro/prieran/fractus/FractusView.java
@@ -10,9 +10,16 @@ import android.os.Build;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static java.lang.Math.atan2;
+import static java.lang.Math.cos;
+import static java.lang.Math.pow;
+import static java.lang.Math.sin;
+import static java.lang.Math.sqrt;
 
 public class FractusView extends View {
     private final List<PointF> points = new ArrayList<>();
@@ -89,5 +96,47 @@ public class FractusView extends View {
     private void init() {
         paint = new Paint();
         paint.setStyle(Paint.Style.STROKE);
+    }
+
+    public void onDoButtonClick() {
+        final PointF first = points.get(0);
+        final float firstX = first.x;
+        final float firstY = first.y;
+
+        final PointF second = points.get(1);
+        final float secondX = second.x;
+        final float secondY = second.y;
+
+        final PointF last = points.get(points.size() - 1);
+        final float lastX = last.x;
+        final float lastY = last.y;
+
+        float coef = (float) sqrt((pow((lastX - firstX), 2) + pow((lastY - firstY), 2)) / (pow((secondX - firstX), 2) + pow((secondY - firstY), 2)));
+
+        double angle = atan2(secondY - firstY, secondX - firstX) - atan2(lastY - firstY, lastX - firstX);
+        Toast.makeText(getContext(), coef + " " + (-angle * 57.2958), Toast.LENGTH_SHORT).show();
+        List<PointF> newPoints = new ArrayList<>();
+        for (int i = 0; i < points.size(); i++) {
+            PointF point = points.get(i);
+            PointF rotatedPoint = rotate(new PointF(point.x - firstX, point.y - firstY), angle);
+            PointF newPoint = new PointF(rotatedPoint.x / coef + firstX, rotatedPoint.y / coef + firstY);
+            newPoints.add(newPoint);
+        }
+        newPoints.size();
+        for (int i = 1; i < newPoints.size(); i++) {
+            PointF oldPoint = newPoints.get(i - 1);
+            PointF newPoint = newPoints.get(i);
+            final Path newPath = new Path();
+            newPath.moveTo(oldPoint.x, oldPoint.y);
+            newPath.lineTo(newPoint.x, newPoint.y);
+            paths.add(newPath);
+        }
+        invalidate();
+    }
+
+    private PointF rotate(PointF point, double angle) {
+        float newX = (float) (point.x * cos(angle) - point.y * sin(angle));
+        float newY = (float) (point.x * sin(angle) + point.y * cos(angle));
+        return new PointF(newX, newY);
     }
 }
--- app/src/main/java/pro/prieran/fractus/MainActivity.java
+++ app/src/main/java/pro/prieran/fractus/MainActivity.java
@@ -2,6 +2,8 @@ package pro.prieran.fractus;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final FractusView fractusView = (FractusView) findViewById(R.id.fractus_view);
+        final Button doButton = (Button) findViewById(R.id.button);
+
+        doButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fractusView.onDoButtonClick();
+            }
+        });
     }
 }
